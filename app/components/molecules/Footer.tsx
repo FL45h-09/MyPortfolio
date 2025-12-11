92c1c247952490981cbd8f4d6dedf5416ffa9f62
FL45h-09 <vishal.ravanank@hotmail.com>
2025-12-11 16:44:30 +0530

    footer container added

--- app/components/molecules/Footer.tsx
+++ app/components/molecules/Footer.tsx
@@ -3,5 +3,9 @@ import { ProfileData as data } from '@/config/content';
 
 
 export const Footer: React.FC = () => (
-    <footer className="footer"><p className='text-base text-center'>&copy; {new Date().getFullYear()} — Built with ❤️ by {data.fname} using Next.js + TypeScript + Tailwind.</p></footer>
+    <footer className="footer">
+        <div className="container py-4.5">
+            <p className='text-base text-center'>&copy; {new Date().getFullYear()} — Built with ❤️ by {data.fname} using Next.js + TypeScript + Tailwind.</p>
+        </div>
+    </footer>
 );
